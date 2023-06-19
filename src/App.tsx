804fd3cb0567cf674159eb79f78eda62d5f63c49
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-06-19 13:04:32 -0300

    Atualização geral

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from './paginas/home/Home';
+import './App.css';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Typography, Grid, Button, Box } from '@material-ui/core';
+import './Home.css';
+
+function Home() {
+    return (
+        // Precisa fazer um container pra retornar somente um elemento aqui (<> é um Fragment):
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://unsplash.com/pt-br/fotografias/Qc7DDVxr734" alt="Imagens Da Natureza, Animais, Imagens De Pássaros, Bico, Melro, Tentilhão" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default Home;
