5584341a3468d14c74ca645f69faf31589601e97
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-06-21 09:38:06 -0300

    Atualização - Criando os Componentes: getAll Temas e getAll Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,22 @@ import Home from './paginas/home/Home';
 import './App.css';
 import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+        <Route path="/temas" element={<ListaTema />} />
+        <Route path="/posts" element={<ListaPostagem />} />
+
       </Routes>
       <Footer />
     </Router>
