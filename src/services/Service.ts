729bee86a567149f448c05c5d0b2f31475d0d88a
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-06-20 21:27:22 -0300

    Atualização - Comunicação com o Back-end (Cadastro - Model e Service)

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,8 +4,13 @@ export const api = axios.create({
     baseURL: 'https://blogpessoal-cg9f.onrender.com'
 })
 
-// /usuarios/logar
-export const Login = async (url: any, dados: any, setDado: any) => {
-    const resposta = await api.post(url, dados)
-    setDado(resposta.data)
-}
+    export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
+        const resposta = await api.post(url, dados)
+        setDado(resposta.data)
+    }
+
+    // /usuarios/logar
+    export const login = async (url: any, dados: any, setDado: any) => {
+        const resposta = await api.post(url, dados)
+        setDado(resposta.data.token)
+    }
